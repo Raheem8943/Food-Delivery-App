eb833ac5d1fb264b87836536f4872ed75cfac615
Raheem8943 <Raheem318558@gmail.com>
2025-12-04 11:42:57 +0530

    class base component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,9 @@
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
-      <h1>About Us</h1>
+      <UserClass />
     </div>
   );
 };
---
+++ src/pages/UserClass.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Raheeme</h1>;
+  }
+}
+export default UserClass;
