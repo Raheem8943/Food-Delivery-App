5889de3f8e1fb6c385dd45b3605848fc07a264e6
Raheem8943 <Raheem318558@gmail.com>
2025-12-16 12:30:52 +0530

    ui updation

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,35 +1,35 @@
 const Shimmer = () => {
   return (
-    <div className="">
+    <div className="animate-pulse">
       <div className="flex gap-20 m-3 p-3">
         <div className="flex gap-2 ">
-          <div className="w-[220px] bg-gray-100 rounded-[5px] h-6"></div>
-          <div className="w-[70px]  bg-gray-100 rounded-[5px]"></div>
+          <div className="w-[220px] bg-gray-300 rounded-[5px] h-6"></div>
+          <div className="w-[70px]  bg-gray-300 rounded-[5px]"></div>
         </div>
-        <div className="w-[100px] bg-gray-100 rounded-[5px]"></div>
+        <div className="w-[300px] bg-gray-300 rounded-[5px]"></div>
       </div>
       <div className="grid grid-cols-7 justify-between m-6 p-4 gap-3 ">
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
-        <div className="flex bg-gray-100 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
+        <div className="flex bg-gray-300 w-[235px] h-[450px] rounded-[5px]"></div>
       </div>
     </div>
   );
--- src/components/ShimmerMenu.jsx
+++ src/components/ShimmerMenu.jsx
@@ -1,16 +1,16 @@
 const ShimmerMenu = () => {
   return (
-    <div className="p-2 m-2 flex flex-col items-center gap-3">
-      <div className="bg-gray-100 w-[930px] h-[285px] rounded-2xl py-3"></div>
-      <div className="bg-gray-100 w-[880px] h-[75px]  rounded-[13px]"></div>
-      <div className="bg-gray-100 w-[880px] h-[75px]  rounded-[13px]"></div>
-      <div className="bg-gray-100 w-[880px] h-[75px]  rounded-[13px]"></div>
-      <div className="bg-gray-100 w-[880px] h-[75px]  rounded-[13px]"></div>
-      <div className="bg-gray-100 w-[880px] h-[75px]  rounded-[13px]"></div>
-      <div className="bg-gray-100 w-[880px] h-[75px]  rounded-[13px]"></div>
-      <div className="bg-gray-100 w-[880px] h-[75px]  rounded-[13px]"></div>
-      <div className="bg-gray-100 w-[880px] h-[75px]  rounded-[13px]"></div>
-      <div className="bg-gray-100 w-[880px] h-[75px]  rounded-[13px]"></div>
+    <div className="p-2 m-2 flex animate-pulse flex-col items-center gap-3">
+      <div className="bg-gray-300 w-[930px] h-[285px] rounded-2xl py-3"></div>
+      <div className="bg-gray-300 w-[880px] h-[75px]  rounded-[13px]"></div>
+      <div className="bg-gray-300 w-[880px] h-[75px]  rounded-[13px]"></div>
+      <div className="bg-gray-300 w-[880px] h-[75px]  rounded-[13px]"></div>
+      <div className="bg-gray-300 w-[880px] h-[75px]  rounded-[13px]"></div>
+      <div className="bg-gray-300 w-[880px] h-[75px]  rounded-[13px]"></div>
+      <div className="bg-gray-300 w-[880px] h-[75px]  rounded-[13px]"></div>
+      <div className="bg-gray-300 w-[880px] h-[75px]  rounded-[13px]"></div>
+      <div className="bg-gray-300 w-[880px] h-[75px]  rounded-[13px]"></div>
+      <div className="bg-gray-300 w-[880px] h-[75px]  rounded-[13px]"></div>
     </div>
   );
 };
